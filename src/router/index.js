38392db909e5c3d7ee216770f2fd68c65ffd3345
Fitria Damayanti <fitria.damayanti.996@gmail.com>
2024-04-21 14:00:45 +0700

    update router

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ItemList from './components/ItemList.vue'
 import ItemDetail from './components/ItemDetail.vue'
+import AddItem from './components/AddItem.vue'
+import EditItem from './components/EditItem.vue'
 
 const routes = [
   {
@@ -12,6 +14,16 @@ const routes = [
     path: '/item/:id',
     name: 'ItemDetail',
     component: ItemDetail
+  },
+  {
+    path: '/add',
+    name: 'AddItem',
+    component: AddItem
+  },
+  {
+    path: '/edit/:id',
+    name: 'EditItem',
+    component: EditItem
   }
 ]
 
