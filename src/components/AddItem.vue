cdb0c88b5bf82d1bfa3c4f74645e9c7e4ccb4ab2
Fitria Damayanti <fitria.damayanti.996@gmail.com>
2024-04-21 15:45:00 +0700

    Add validation whe create new item

--- src/components/AddItem.vue
+++ src/components/AddItem.vue
@@ -5,6 +5,7 @@
       <div class="mb-3">
         <label for="title" class="form-label">Title</label>
         <input type="text" class="form-control" id="title" v-model="form.title" required />
+        <span class="text-danger" v-if="errors.title">{{ errors.title }}</span>
       </div>
       <div class="mb-3">
         <label for="description" class="form-label">Description</label>
@@ -14,6 +15,7 @@
           v-model="form.description"
           required
         ></textarea>
+        <span class="text-danger" v-if="errors.description">{{ errors.description }}</span>
       </div>
       <button type="submit" class="btn btn-primary">Add</button>
     </form>
@@ -27,25 +29,40 @@ import { useStore } from 'vuex'
 
 export default {
   name: 'AddItem',
-  emits: ['item-added'], // Define custom event
   data() {
     return {
       form: {
         title: '',
         description: ''
-      }
+      },
+      errors: {}
     }
   },
   methods: {
     async addItem() {
-      await this.$store.dispatch('addItem', {
-        title: this.form.title,
-        body: this.form.description
-      })
-      this.form.title = ''
-      this.form.description = ''
-      this.$router.push({ name: 'ItemList' })
-      this.$emit('item-added') // Emit custom event after adding item
+      if (this.isFormValid()) {
+        await this.$store.dispatch('addItem', {
+          title: this.form.title,
+          body: this.form.description
+        })
+        this.form.title = ''
+        this.form.description = ''
+        this.$router.push({ name: 'ItemList' })
+        this.$emit('item-added') // Emit custom event after adding item
+      }
+    },
+    isFormValid() {
+      this.errors = {}
+
+      if (!this.form.title) {
+        this.errors.title = 'Title is required.'
+      }
+
+      if (!this.form.description) {
+        this.errors.description = 'Description is required.'
+      }
+
+      return Object.keys(this.errors).length === 0
     }
   }
 }
