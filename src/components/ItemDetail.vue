f12ac6b73daab2bdac7b85ceb7014a0e00477424
Fitria Damayanti <fitria.damayanti.996@gmail.com>
2024-04-21 14:04:10 +0700

    update item detail

--- src/components/ItemDetail.vue
+++ src/components/ItemDetail.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     <h1>{{ item.title }}</h1>
-    <div>{{ item.body }}</div>
+    <p>{{ item.body }}</p>
   </div>
 </template>
 
 <script>
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+import { useStore } from 'vuex'
+
 export default {
   name: 'ItemDetail',
   data() {
@@ -13,15 +17,11 @@ export default {
       item: {}
     }
   },
-  methods: {
-    async fetchItem() {
-      const itemId = this.$route.params.id
-      const response = await fetch(`https://jsonplaceholder.typicode.com/posts/${itemId}`)
-      this.item = await response.json()
-    }
-  },
-  created() {
-    this.fetchItem()
+  async created() {
+    const store = useStore()
+    const route = useRoute()
+    const itemId = route.params.id
+    this.item = store.state.items.find((item) => item.id == itemId)
   }
 }
 </script>
